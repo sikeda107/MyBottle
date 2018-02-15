d788b968e0268bfdf7f588b7d6ab16c2d088affb
Sho Ikeda <wakame1121@gmail.com>
2018-02-16 02:49:17 +0900

    modify README

--- hello.py
+++ hello.py
@@ -1,6 +1,9 @@
 from bottle import route, run
 
-@route('/hello')
-def hello():
-    return "Hello World"
+@route('')
+@route('/hello/<name>')
+def great(name='stranger'):
+    return template('Hello {{name}}, how are you?',name =name
+    )
+
 run(host='localhost',port=8080,debug=True)
